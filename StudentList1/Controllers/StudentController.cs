264162068dffe4b8a402044ac4524c4020ce0d3e
Onkar-Creator <dongareonkar007@gmail.com>
2022-11-11 19:22:21 +0530

    Add project files.

---
+++ StudentList1/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentList1.Models;
+namespace StudentList1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public static List<Student> students = new List<Student>();
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Student student = new Student();
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            students.Add(student);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+    }
+}
---
+++ StudentList1/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentList1.Models
+{
+    public class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Standard { get; set; }
+    }
+}
